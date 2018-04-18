93e95e355ddd81b1031fc7b89d8b29a565a12458
Sai Aditya Varma <aditya.mudunuri@yahoo.com>
2018-04-18 03:31:57 -0400

    /customers: Added tabs in Order History and made few changes in the Models

--- app/src/main/java/com/example/vamshedhar/androidpos/objects/Customer.java
+++ app/src/main/java/com/example/vamshedhar/androidpos/objects/Customer.java
@@ -8,17 +8,32 @@ import android.support.annotation.NonNull;
 
 public class Customer extends CreatedTimeObject implements Comparable<Customer> {
 
-    private String id, name, email, created_user;
+    private String id;
+    private String name;
+    private String email;
+    private String created_user;
+    private String phone_no;
+
+    public Customer(String id, String name, String email, String created_user, String phone_no) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.created_user = created_user;
+        this.phone_no = phone_no;
+    }
+
+
 
     public Customer() {
 
     }
 
-    public Customer(String id, String name, String email, String created_user) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.created_user = created_user;
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
     }
 
     public String getId() {
@@ -55,7 +70,11 @@ public class Customer extends CreatedTimeObject implements Comparable<Customer> 
 
     @Override
     public String toString() {
-        return "Customer{" + "name='" + name + '\'' + ", email='" + email + '\'' + '}';
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone_no='" + phone_no + '\'' +
+                '}';
     }
 
     @Override
